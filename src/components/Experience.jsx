5c3f625ca0360ba6710e6864bd281d81fa72e7c1
freab <fremesfin1@gmail.com>
2025-10-25 22:09:43 +0300

    fixed room , wall , floor and roof

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -3,18 +3,26 @@ import PCScreen from "./screen";
 import PCCamera from "./PCCamera";
 import { Environment } from "@react-three/drei";
 import Room from "./Room";
+import Wall from "./wall";
+import MainCamera from "./MainCamera";
+import Floor from "./floor";
+import Roof from "./roof";
 export const Experience = () => {
   return (
     <>
       {/* <OrbitControls /> */}
-      <Environment preset="dawn"/>
+      <Environment preset="dawn" environmentIntensity={0.3} />
       {/* <mesh>
         <boxGeometry />
         <meshNormalMaterial />
       </mesh> */}
       <Room />
       <PCScreen />
-      <PCCamera />   
+      <PCCamera />
+      <Wall />
+      <MainCamera />
+      <Floor />
+      <Roof />
     </>
   );
 };
