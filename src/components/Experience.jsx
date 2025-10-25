1e3eaf9be339c9509be268c6d08bca9755189304
brooksolomon <officialbrooksolo@gmail.com>
2025-10-25 09:52:02 +0300

    model cleanup

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,18 +1,18 @@
 import { OrbitControls } from "@react-three/drei";
-import PC from "./PC";
 import PCScreen from "./screen";
 import PCCamera from "./PCCamera";
 import { Environment } from "@react-three/drei";
+import Room from "./Room";
 export const Experience = () => {
   return (
     <>
-      {/* <OrbitControls /> */}
+      <OrbitControls />
       <Environment preset="dawn"/>
       {/* <mesh>
         <boxGeometry />
         <meshNormalMaterial />
       </mesh> */}
-      <PC />
+      <Room />
       <PCScreen />
       <PCCamera />   
     </>
--- src/components/screen.jsx
+++ src/components/screen.jsx
@@ -24,11 +24,11 @@ export function PCScreen(props) {
           transform
           wrapperClass="screen-html"
           distanceFactor={0.5}
-          position={[0.01, 0, 0.01]}
+          position={[0.07, -0.02, 0.01]}
           rotation={[0, -Math.PI,0]}
           style={{
-            width: `${screenWidth * 1075}px`,
-            height: `${screenHeight * 1120}px`,
+            width: `${screenWidth * 1090}px`,
+            height: `${screenHeight * 1150}px`,
             overflow: 'hidden',
             pointerEvents: 'auto',
           }}
